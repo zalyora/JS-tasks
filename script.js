7169b48658179064498da1a04a8b1a3283401194
Olena Lazarenko <alyonalazarenko27@gmail.com>
2023-03-30 05:44:52 +0300

    show calculation results in DOM

--- script.js
+++ script.js
@@ -1,77 +1,108 @@
-const FIRST_NUMBER = prompt('Enter a number:');
-const SECOND_NUMBER = prompt('Enter a number:');
+const FIRST_NUMBER = document.getElementById('first_number');
+const SECOND_NUMBER = document.getElementById('second_number');
+const CALCULATE = document.querySelector('.calc');
+const NUM_RESULT = document.querySelector('.num_result');
 const message = 'I need numbers to calculate';
 const message2 = 'Error: impossible to divide!';
+const confirmMessage = 'Are you sure you want to continue?';
 const sumMessage = 'The sum is ';
 const substrMessage = 'The substraction result is ';
 const multMessage = 'The multiplication result is ';
-const divisMessage = 'The division is ';
+const divisMessage = 'The division result is ';
 
-if (!FIRST_NUMBER || !SECOND_NUMBER) {
-    alert(message);
-} else if (SECOND_NUMBER == 0 && FIRST_NUMBER > SECOND_NUMBER) {
-    alert(`${sumMessage}${sum(FIRST_NUMBER, SECOND_NUMBER)}`);
-    alert(`${substrMessage}${substract(FIRST_NUMBER, SECOND_NUMBER)}`);
-    alert(`${multMessage}${multiply(FIRST_NUMBER, SECOND_NUMBER)}`);
-    alert(message2);
-} else if (SECOND_NUMBER == 0 && FIRST_NUMBER < SECOND_NUMBER) {
-    let CONFIRMATION = confirm('Are you sure you want to continue?');
-    if (!CONFIRMATION) {
-        alert(`${sumMessage}${sum(FIRST_NUMBER, SECOND_NUMBER)}`);
-        alert(`${multMessage}${multiply(FIRST_NUMBER, SECOND_NUMBER)}`);
-        alert(message2);
+CALCULATE.addEventListener('click', () => {
+    NUM_RESULT.innerHTML = '';
+    initCalc();
+    clearInputs();
+});
+
+function clearInputs() {
+    FIRST_NUMBER.value = '';
+    SECOND_NUMBER.value = '';
+}
+
+function initCalc() {
+    let first = FIRST_NUMBER.value;
+    let second = SECOND_NUMBER.value;
+    let sumResult = `<span>${sumMessage}</span><span class="num">${sum(first, second)}.</span>`;
+    let substrResult = `<span>${substrMessage}</span><span class="num">${substract(first, second)}.</span>`;
+    let multResult = `<span>${multMessage}</span><span class="num">${multiply(first, second)}.</span>`;
+    let divisResult = `<span>${divisMessage}</span><span class="num">${divide(first, second)}.</span>`;
+    let listItems = [];
+    if (!first || !second) {
+        alert(message);
+    } else if ((second == 0 && first > second) || (second == 0 && first == second)) {
+        listItems.push(sumResult, substrResult, multResult, message2);
+    } else if (second == 0 && first < second) {
+        let CONFIRMATION = confirm(confirmMessage);
+        if (!CONFIRMATION) {
+            listItems.push(sumResult, multResult, message2);
+        } else {
+            listItems.push(sumResult, substrResult, multResult, message2);
+        }
+    } else if (first < second) {
+        let CONFIRMATION = confirm(confirmMessage);
+        if (!CONFIRMATION) {
+            listItems.push(sumResult, multResult, message2);
+        } else {
+            listItems.push(sumResult, substrResult, multResult, divisResult);
+        }
     } else {
-        alert(`${sumMessage}${sum(FIRST_NUMBER, SECOND_NUMBER)}`);
-        alert(`${substrMessage}${substract(FIRST_NUMBER, SECOND_NUMBER)}`);
-        alert(`${multMessage}${multiply(FIRST_NUMBER, SECOND_NUMBER)}`);
-        alert(message2);
+        listItems.push(sumResult, substrResult, multResult, divisResult);
     }
-} else if (FIRST_NUMBER < SECOND_NUMBER) {
-    let CONFIRMATION = confirm('Are you sure you want to continue?');
-    if (!CONFIRMATION) {
-        alert(`${sumMessage}${sum(FIRST_NUMBER, SECOND_NUMBER)}`);
-        alert(`${multMessage}${multiply(FIRST_NUMBER, SECOND_NUMBER)}`);
-        alert(`${divisMessage}${divide(FIRST_NUMBER, SECOND_NUMBER)}`);
-    } else {
-        alert(`${sumMessage}${sum(FIRST_NUMBER, SECOND_NUMBER)}`);
-        alert(`${substrMessage}${substract(FIRST_NUMBER, SECOND_NUMBER)}`);
-        alert(`${multMessage}${multiply(FIRST_NUMBER, SECOND_NUMBER)}`);
-        alert(`${divisMessage}${divide(FIRST_NUMBER, SECOND_NUMBER)}`);
+    createListItems();
+    
+    function createListItems() {
+        for (let i = 0; i < listItems.length; i++) {
+            const li = document.createElement('li');
+            li.style.listStyle = 'none';
+            li.style.fontSize ='36px';
+            li.style.fontWeight = 'bold';
+            if (listItems[i] == message2) {
+                li.innerHTML = `<span>${listItems[i]}</span><span></span>`;
+            } else {
+                li.innerHTML = listItems[i];
+            }
+            let elem = li.children[1];
+            let color = parseFloat(elem.innerHTML);
+            if (color > 0) {
+                li.style.color = 'green';
+            } else {
+                li.style.color = 'red';
+            }
+            NUM_RESULT.appendChild(li);
+        }
     }
-} else {
-    alert(`${sumMessage}${sum(FIRST_NUMBER, SECOND_NUMBER)}`);
-    alert(`${substrMessage}${substract(FIRST_NUMBER, SECOND_NUMBER)}`);
-    alert(`${multMessage}${multiply(FIRST_NUMBER, SECOND_NUMBER)}`);
-    alert(`${divisMessage}${divide(FIRST_NUMBER, SECOND_NUMBER)}`);
+}
+
+
+function isNumber(x) {
+    if (typeof x !== 'number') {
+        return Number(x);
+    }
+    return x;
 }
 
 function sum(a, b) {
-    if (typeof a === 'number' && typeof b === 'number') {
-        return a + b;
-    } else {
-        return Number(a) + Number(b);
-    }
+    a = isNumber(a);
+    b = isNumber(b);
+    return a + b;
 }
 
 function substract(a, b) {
-    if (typeof a === 'number' && typeof b === 'number') {
-        return a - b;
-    } else {
-        return Number(a) - Number(b);
-    }
+    a = isNumber(a);
+    b = isNumber(b);
+    return a - b;
 }
 
 function multiply(a, b) {
-    if (typeof a === 'number' && typeof b === 'number') {
-        return a * b;
-    } else {
-        return Number(a) * Number(b);
-    }
+    a = isNumber(a);
+    b = isNumber(b);
+    return a * b;
 }
 
 function divide(a, b) {
-    if (typeof a === 'number' && typeof b === 'number') {return a / b;
-    } else {
-        return Number(a) / Number(b);
-    }
+    a = isNumber(a);
+    b = isNumber(b);
+    return a / b;
 }
--- script1.js
+++ script1.js
@@ -1,8 +1,64 @@
-const array = [3, 57, 9, 45, '293', 12, 6, NaN, 54, 5];
-const str = '#';
+const defaultArray = [3, 57, 9, 45, '293', 12, 6, NaN, 54, 5, -1];
+const BRANCHES_NUMBER = document.querySelector('#branches_number');
+const DRAW = document.querySelector('.draw-btn');
+const FIR_TREE = document.querySelector('.fir_tree');
+const ARRAY = document.querySelector('#array');
+const ARR_CALC = document.querySelector('.arr_calc');
+const ARR_RESULT = document.querySelector('.arr_result');
+
+function ToArray() {
+    this.inputArray = [];
+}
+
+ToArray.prototype.addItem = function (item) {
+    this.inputArray.push(item);
+}
+
+ToArray.prototype.printItems = function () {
+    ARR_RESULT.innerHTML = '';
+    const li = document.createElement('li');
+    li.style.listStyle = 'none';
+    for (let i = 0; i < this.inputArray.length; i++) {
+        createSpan(li, this.inputArray[i], 0);
+        ARR_RESULT.appendChild(li);
+    }
+}
+
+const toArray = new ToArray();
+ARR_CALC.addEventListener('click', () => {
+    ARR_RESULT.innerHTML = '';
+    let inputArray = Object.values(toArray);
+    if (inputArray[0].length !== 0) {
+        arraySum(...inputArray);
+        minMax(...inputArray);
+        printArray(...inputArray, true);
+        clearInput(ARRAY);
+    } else {
+        arraySum(defaultArray);
+        minMax(defaultArray);
+        printArray(defaultArray, false);
+        clearInput(ARRAY);
+    }
+});
+
+ARRAY.addEventListener('keyup', (e) => {
+    if (e.key === "Enter") {
+        let item;
+        if (isNaN(Number(ARRAY.value))) {
+            item = ARRAY.value;
+        } else {
+            item = Number(ARRAY.value);
+        }
+        toArray.addItem(item);
+        toArray.printItems();
+        clearInput(ARRAY);
+    }
+});
 
 function arraySum(arr) {
+    ARR_RESULT.innerHTML = '';
     let sum = 0;
+    const message = 'The sum of numbers in the array is ';
     for (let i = 0; i < arr.length; i++) {
         if (typeof arr[i] !== 'number' || isNaN(arr[i])) {
             continue;
@@ -10,11 +66,16 @@ function arraySum(arr) {
             sum = sum + arr[i];
         }
     }
-    console.log(sum);
+    const li = document.createElement('li');
+    li.innerText = `${message}${sum}.`;
+    styleListItem(li);
+    ARR_RESULT.appendChild(li);
 }
 
-function minMax(array) {
-    let checkedArray = numbersArray(array);
+function minMax(arr) {
+    let checkedArray = numbersArray(arr);
+    let minMessage = 'The min number is ';
+    let maxMessage = 'The max number is ';
     let maxNumber = checkedArray[0];
     let minNumber = checkedArray[0];
     for (let i = 0; i < checkedArray.length; i++) {
@@ -27,13 +88,96 @@ function minMax(array) {
             maxNumber = checkedArray[i];
         }
     }
-console.log(minNumber, maxNumber);
+    const li = document.createElement('li');
+    createSpan(li, `${minMessage}${minNumber}.`, minNumber);
+    createSpan(li, ` ${maxMessage}${maxNumber}.`, maxNumber);
+    styleListItem(li);
+    ARR_RESULT.appendChild(li);
 }
 
-function draw(number) {
-    for (let i = 0; i < number; i++) {
-        console.log(str.repeat(i));
+function getColor(num) {
+    if (num > 0) {
+        return 'green';
+    } else if(num < 0) {
+        return 'red';
+    } else {
+        return 'black';
     }
+}
+
+function styleListItem(elem) {
+    elem.style.listStyle = 'none';
+    elem.style.fontSize ='36px';
+    elem.style.fontWeight = 'bold';
+}
+
+function createSpan(elem, text, number) {
+    const span = document.createElement('span');
+    span.textContent = text;
+    span.style.display = 'inline-block';
+    span.style.marginRight = '10px';
+    span.style.color = getColor(number);
+    span.style.opacity = setOpacity(number);
+    elem.appendChild(span);
+}
+
+function printArray(arr, bool) {
+    const li = document.createElement('li');
+    li.style.listStyle = 'none';
+    if (bool) {
+        li.innerText = 'Your array: ';
+    } else {
+        li.innerText = 'Default array: ';
+    }
+    for (let i = 0; i < arr.length; i++) {
+        createSpan(li, arr[i], 0);
+    }
+    ARR_RESULT.insertAdjacentElement("afterbegin", li);
+}
+
+DRAW.addEventListener('click', () => {
+    draw();
+    clearInput(BRANCHES_NUMBER);
+});
+
+function draw() {
+    const str = '#';
+    FIR_TREE.innerHTML = '';
+    let number = BRANCHES_NUMBER.value;
+    for (let i = 0; i < number; i++) {
+        let string = str.repeat(i + 1);
+        const li = document.createElement('li');
+        li.innerText = string;
+        styleListItem(li);
+        li.style.color = 'green';
+        li.style.textAlign = 'center';
+        FIR_TREE.appendChild(li);
+    }
+}
+
+function setOpacity(num) {
+    num = Math.abs(num);
+    switch (true) {
+        case num > 80 && num <= 100:
+            num = 0.9;
+            break;
+        case num > 60 && num <= 80:
+            num = 0.8;
+            break;
+        case num > 40 && num <= 60:
+            num = 0.7;
+            break;
+        case num > 20 && num <= 40:
+            num = 0.6;
+            break;
+        case num > 0 && num <= 20:
+            num = 0.5;
+            break;
+        default:
+            num = 1;
+            break;
+    }
+    return num;
 }
 
 function numbersArray(array) {
@@ -46,4 +190,8 @@ function numbersArray(array) {
         }
     }
     return checkedArray;
+}
+
+function clearInput(input) {
+    input.value = '';
 }
